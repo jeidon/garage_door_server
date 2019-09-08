9eab25ac34419782d085e377d0865801d8bf0326
Luke Dupin <lukedupin@gmail.com>
2019-09-07 23:24:15 -0700

    Deleted magic keys from settings

--- pysite/example.settings.py
+++ pysite/example.settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%)a-$p_suu8+59hc8%ld_4#$+)q6et2*k#5ds)m%v=_*7_dc*j'
+SECRET_KEY = 'REPLACE WITH REAL KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
